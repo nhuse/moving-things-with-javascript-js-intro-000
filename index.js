151ede127fd3057f0262ce3079b15275057b5235
Nathan Huse <nathan.huse@yahoo.com>
2021-06-15 15:22:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,15 +2,6 @@ const app = "I don't do much."
 var dodger = document.getElementById('dodger')
 dodger.style.backgroundColor = "#FF69B4"
 
-document.addEventListener('keydown', function(e) {
-  if (e.which === 37) {
-    moveDodgerLeft();
-  }
-  else if (e.which === 39) {
-    moveDodgerRight();
-  }
-})
-
 function moveDodgerLeft() {
   var leftNumbers = dodger.style.left.replace('px', '')
   var left = parseInt(leftNumbers, 10)
@@ -28,3 +19,12 @@ function moveDodgerRight() {
     dodger.style.right = `${right - 10}px`
   }
 }
+
+document.addEventListener('keydown', function(e) {
+  if (e.which === 37) {
+    moveDodgerLeft();
+  }
+  else if (e.which === 39) {
+    moveDodgerRight();
+  }
+})
