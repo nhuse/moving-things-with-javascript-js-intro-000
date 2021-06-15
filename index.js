0e3a6cf24c04e371c58a2d6e45ac1fc1e7351104
Nathan Huse <nathan.huse@yahoo.com>
2021-06-15 15:15:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,10 +21,10 @@ function moveDodgerLeft() {
 }
 
 function moveDodgerRight() {
-  var rightNumbers = dodger.style.right.replace('px', '')
-  var right = parseInt(rightNumbers, 10)
-  
-  if (right > 0) {
-    dodger.style.right = `${right - 10}px`
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
+    
+  if (left < 0) {
+    dodger.style.left = `${left + 10}px`
   }
 }
