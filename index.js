d2152cde7fa6b0043c8dffd0d41773ca59c9d9b1
Nathan Huse <nathan.huse@yahoo.com>
2021-06-15 15:18:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ document.addEventListener('keydown', function(e) {
   if (e.which === 37) {
     moveDodgerLeft()
   }
-  else if (e.which === 39) {
+  if (e.which === 39) {
     moveDodgerRight()
   }
 })
@@ -21,10 +21,10 @@ function moveDodgerLeft() {
 }
 
 function moveDodgerRight() {
-  var leftNumbers = dodger.style.left.replace('px', '')
-  var left = parseInt(leftNumbers, 10)
-    
-  if (left < 360) {
-    dodger.style.left = `${left + 10}px`
+  var rightNumbers = dodger.style.right.replace('px', '')
+  var right = parseInt(rightNumbers, 10)
+  
+  if (right < 400) {
+    dodger.style.right = `${right - 10}px`
   }
 }
